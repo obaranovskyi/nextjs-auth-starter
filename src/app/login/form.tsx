efaba5431f36f321a79569323c576c8c7d25a8dd
olegbaranovskyi <153509142+olegbaranovskyi@users.noreply.github.com>
2024-04-06 17:51:47 +0300

    Auto git push ...

--- src/app/login/form.tsx
+++ src/app/login/form.tsx
@@ -2,64 +2,87 @@
 
 import styles from './form.module.css';
 import Form from 'react-bootstrap/Form';
+import Alert from 'react-bootstrap/Alert';
 import Col from 'react-bootstrap/Col';
 import Button from 'react-bootstrap/Button';
 import { useState } from 'react';
+import axios from 'axios';
+import { useRouter } from 'next/navigation';
 
 
 export default function LoginForm() {
-  const [email, handleEmailChange] = useState('');
+  const [username, handleUsernameChange] = useState('');
   const [password, handlePasswordChange] = useState('');
   const [validated, setValidated] = useState(false);
+  const [isError, setIsError] = useState(false);
+  const router = useRouter();
 
-  const handleSubmit = (event: any) => {
+  const handleSubmit = async(event: any) => {
+    event.preventDefault();
+    event.stopPropagation();
+
     const form = event.currentTarget;
-    if (form.checkValidity() === false) {
-      event.preventDefault();
-      event.stopPropagation();
-    }
 
     setValidated(true);
 
-    console.log('here', email, password)
+    if (form.checkValidity() === false) return;
 
+    try {
+      const { data } = await axios.post(
+        'http://localhost:3333/api/auth/login',
+        { username: username, password }
+      );
+
+      localStorage.setItem('user', JSON.stringify(data));
+      
+      router.push('/')
+
+    } catch(err) {
+      setIsError(true);
+    }
   };
 
   return (
-    <Form noValidate validated={validated} onSubmit={handleSubmit}>
-      <Form.Group className="mb-3" as={Col} controlId="formEmail">
-        <Form.Label>Email address</Form.Label>
-        <Form.Control
-          type="email"
-          placeholder="Enter email"
-          bsPrefix={`form-control ${styles['input-width']}`}
-          value={email}
-          onChange={event => handleEmailChange(event.target.value)}
-          autoComplete="off"
-          required
-        />
-        <Form.Control.Feedback type="invalid">
-          Please provide a valid email.
-        </Form.Control.Feedback>
-      </Form.Group>
-      <Form.Group className="mb-3" as={Col} controlId="formPassword">
-        <Form.Label>Password</Form.Label>
-        <Form.Control
-          type="password"
-          placeholder="Password"
-          bsPrefix={`form-control ${styles['input-width']}`}
-          value={password}
-          onChange={event => handlePasswordChange(event.target.value)}
-          autoComplete="off"
-          required
-        />
-        <Form.Control.Feedback type="invalid">
-          Please provide a valid password.
-        </Form.Control.Feedback>
-      </Form.Group>
-      <Button variant="primary" type="submit">
-        Login
-      </Button>
-    </Form>
+    <div>
+      {isError && <Alert key='danger' variant="danger">
+        Invalid username or password.
+      </Alert>}
+
+      <Form noValidate validated={validated} onSubmit={handleSubmit}>
+        <Form.Group className="mb-3" as={Col} controlId="formUsername">
+          <Form.Label>Username</Form.Label>
+          <Form.Control
+            type="text"
+            placeholder="Enter username"
+            bsPrefix={`form-control ${styles['input-width']}`}
+            value={username}
+            onChange={event => handleUsernameChange(event.target.value)}
+            autoComplete="off"
+            required
+          />
+          <Form.Control.Feedback type="invalid">
+            Please provide a valid username.
+          </Form.Control.Feedback>
+        </Form.Group>
+        <Form.Group className="mb-3" as={Col} controlId="formPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            type="password"
+            placeholder="Password"
+            bsPrefix={`form-control ${styles['input-width']}`}
+            value={password}
+            onChange={event => handlePasswordChange(event.target.value)}
+            autoComplete="off"
+            required
+          />
+          <Form.Control.Feedback type="invalid">
+            Please provide a valid password.
+          </Form.Control.Feedback>
+        </Form.Group>
+        <Button variant="primary" type="submit">
+          Login
+        </Button>
+      </Form>
+    </div>
   );
 }
